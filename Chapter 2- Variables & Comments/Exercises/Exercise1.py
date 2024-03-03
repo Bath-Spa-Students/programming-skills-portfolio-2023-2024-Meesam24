bf0932da6ab5524343deb66f177b4bf724b47a6d
Meesam Abbas <MeesumNotFound@Gmail.Com>
2024-03-03 21:17:15 +0400

    Exercise1.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise1.py
@@ -0,0 +1,12 @@
+# Assigning a message to a variable
+message = "How are you today?"
+
+# Printing the initial message
+print("Initial message:", message)
+
+# Changing the value of the variable to a new message
+message = "I hope you're having a great day!"
+
+# Printing the new message
+print("New message:", message)
+
