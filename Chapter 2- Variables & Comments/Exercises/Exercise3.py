276e000e7c06761bc3ecb7e584c06dbe105fee96
Meesam Abbas <MeesumNotFound@Gmail.Com>
2024-03-03 21:17:42 +0400

    Exercise3.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise3.py
@@ -0,0 +1,10 @@
+# Define the person's name with whitespace characters
+person_name = "\t\t John Doe \n"
+
+# Print the name with whitespace
+print("Name with whitespace:", person_name)
+
+# Print the name using each stripping function
+print("Name using lstrip():", person_name.lstrip())
+print("Name using rstrip():", person_name.rstrip())
+print("Name using strip():", person_name.strip())
