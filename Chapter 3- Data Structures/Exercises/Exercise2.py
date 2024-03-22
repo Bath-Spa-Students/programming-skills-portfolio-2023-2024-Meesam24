1d7b4d944e83cd8bcefab9a92c93fbe7b90ba5af
Meesam Abbas <MeesumNotFound@Gmail.Com>
2024-03-23 03:24:05 +0400

    Exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise2.py
@@ -0,0 +1,6 @@
+# Store the names of your friends in a list called names
+names = ["Arthur", "John", "Max", "Peter"]
+
+# Print a personalized message to each friend
+for name in names:
+    print(f"Hello, {name}! How are things going?")
