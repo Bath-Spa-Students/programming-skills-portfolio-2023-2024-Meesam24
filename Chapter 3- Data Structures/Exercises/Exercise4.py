018b87c1c89b3be3782da775eec35a16d6c553a0
Meesam Abbas <MeesumNotFound@Gmail.Com>
2024-03-23 03:24:27 +0400

    Exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise4.py
@@ -0,0 +1,6 @@
+# List of people to invite to dinner
+guest_list = ["Arthur Morgan", "John Marston", "Peter Parker"]
+
+# Print invitation message to each person
+for guest in guest_list:
+    print(f"Dear {guest},\n\nI would like to invite you to dinner. It would be an honor to have you join us.\n\nSincerely, [Meesam Abbas]")
