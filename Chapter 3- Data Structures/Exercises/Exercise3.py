765871fb7a7275d7fcdd9cdb943242b21506d187
Meesam Abbas <MeesumNotFound@Gmail.Com>
2024-03-23 03:24:17 +0400

    Exercise3.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise3.py
@@ -0,0 +1,6 @@
+# List of favorite modes of transportation
+transportation = ["motorcycle", "car", "bicycle", "train"]
+
+# Print statements about each item in the list
+for item in transportation:
+    print(f"I would like to own a {item}.")
