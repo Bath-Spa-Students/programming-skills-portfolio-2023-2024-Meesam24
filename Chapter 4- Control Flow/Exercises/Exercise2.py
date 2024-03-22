f29a2433b341babe6405bf4a2cd6df6b7ca9311b
Meesam Abbas <MeesumNotFound@Gmail.Com>
2024-03-23 03:25:35 +0400

    Exercise2.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise2.py
@@ -0,0 +1,15 @@
+# Version that runs the if block
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("Congratulations! You just earned 5 points for shooting the green alien.")
+else:
+    print("Congratulations! You just earned 10 points.")
+
+# Version that runs the else block
+alien_color = 'red'
+
+if alien_color == 'green':
+    print("Congratulations! You just earned 5 points for shooting the green alien.")
+else:
+    print("Congratulations! You just earned 10 points.")
