6b1d85c9f90ac02e15a605fa718d599856097213
Meesam Abbas <MeesumNotFound@Gmail.Com>
2024-03-23 03:25:24 +0400

    Exercise1.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise1.py
@@ -0,0 +1,11 @@
+# Version that passes the if test
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("Congratulations! You just earned 5 points.")
+
+# Version that fails the if test
+alien_color = 'red'
+
+if alien_color == 'green':
+    print("Congratulations! You just earned 5 points.")
